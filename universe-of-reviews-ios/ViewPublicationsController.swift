c4ec3feb619c644ceab2e6bf7e90773e6ef13740
Eduardo Rodriguez <eduardo.rodriguez@ericher.com>
2019-06-04 02:02:43 -0500

    Design Model
    Views of Login, Reviews and Reviews Detail added to MainStoryboard.

---
+++ universe-of-reviews-ios/ViewPublicationsController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewPublicationsController.swift
+//  universe-of-reviews-ios
+//
+//  Created by Sistemas on 6/4/19.
+//  Copyright © 2019 Guillermo Abneed Rodriguez Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewPublicationsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

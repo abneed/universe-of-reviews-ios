f5fa58ef6d18acc5637d35e4163c1707c1948e5b
Abneed <memoboy262@gmail.com>
2019-06-03 21:37:14 -0500

    Initialize repository

---
+++ universe-of-reviews-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  universe-of-reviews-ios
+//
+//  Created by Guillermo Abneed Rodriguez Velazquez on 6/3/19.
+//  Copyright © 2019 Guillermo Abneed Rodriguez Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
